cccc0df3f9f1a2aab8648157acd3cbc3e35a20a4
Namtii <hit4@o2.pl>
2026-04-20 23:27:54 +0200

    Update filter method scrimba.js

--- filter method scrimba.js
+++ filter method scrimba.js
@@ -1,18 +1,77 @@
-const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18]
 
-// const adults = ages.filter(function(age){
-// /*
-// Challenge:
-// 1. Get rid of the if statement and reduce the 
-//    logic in this function to just one line 
-//    of code.
-// */
-//     if (age >= 18){
-//         return true
-//     }
-// })
+const series = [
+    {
+        name: 'The Wire',
+        location: 'Baltimore',
+        lengthInHours : 60,
+        genres: ['action', 'thriller', 'detective', 'suspense']
+    },
+    {
+        name: 'Game of Thromes',
+        location: 'Westeros and Essos',
+        lengthInHours : 70.25,
+        genres: ['fantasy', 'action', 'tragedy']
+    },
+    {
+        name: 'Friends',
+        location: 'New York',
+        lengthInHours : 85,
+        genres: ['comedy', 'romance', 'drama']
+    },
+    {
+        name: 'The Walking Dead',
+        location: 'Atlanta',
+        lengthInHours : 131,
+        genres: ['zombie', 'apocalypse', 'thriller', 'suspense']
+    },
+    {
+        name: 'The Big Bang Theory',
+        location: 'Pasadena',
+        lengthInHours : 139.66,
+        genres: ['comedy', 'nerd', 'romance']
+    },
+]
 
-const adults = ages.filter(function(age){return age >= 18})
+const newYorkSeries = series.filter(function(show){
+    return show.location === 'New York'
+})
+
+console.log(newYorkSeries)
+
+/*
+Challenge:
+1. Use the .filter() method to create an array
+   of all of the thrillers.
+*/
+const thrillers = series.filter(function(show){
+    return show.genres.includes('thriller')
+})
+
+console.log(thrillers)
 
 
-console.log(adults)
+
+
+
+
+
+
+
+// const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18]
+
+// // const adults = ages.filter(function(age){
+// // /*
+// // Challenge:
+// // 1. Get rid of the if statement and reduce the 
+// //    logic in this function to just one line 
+// //    of code.
+// // */
+// //     if (age >= 18){
+// //         return true
+// //     }
+// // })
+
+// const adults = ages.filter(function(age){return age >= 18})
+
+
+// console.log(adults)
